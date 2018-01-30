9a43a9510ccf8a3448ec95e73c6d6d96b6d888f2
michaeldurfey <mdurfey@me.com>
2018-01-29 20:50:25 -0800

    stuff has passed!

--- src/views/app.js
+++ src/views/app.js
@@ -9,7 +9,8 @@ var AppView = Backbone.View.extend({
 
 
   render: function() {
-    this.$el.html(this.template());
+    this.$el.html(this.template()); 
+    new VideoListView({collection: this.collection}).render();
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,16 +1,18 @@
 var VideoListView = Backbone.View.extend({
-  
+  initialize: function() {
+    this.render();
 
+  },
+  
   render: function() {
-    this.$el.empty();
     this.$el.children().detach();
-    console.log(this.collection);
     this.collection.forEach(this.renderVideo, this);
     this.$el.html(this.template());
+    this.renderVideo();
   },
 
   renderVideo: function(video) {
-    
+    var movie = new Video({exampleVideoData});
     var videoView = new VideoListEntryView({model: movie});
     this.$el.append(videoView.render());
   },
